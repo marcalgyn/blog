bf9322b9b8eab1d87038031d6a8305801a7e8193
marcalgyn <marclgyn@hotmail.com>
2019-10-29 21:46:51 -0300

    Atualizacao cadastro de categoria

--- app.js
+++ app.js
@@ -6,19 +6,25 @@ const bodyParser = require('body-parser')
 const app = express()
 const admin = require('./routes/admin')
 const path = require('path')
-
-
-//consr mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 //Configurações
+    
     //Bory Parser
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
+    
     //Handlebars
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set('view engine', 'handlebars')
+   
     //Mangoose
-      //em breve
+      mongoose.Promise = global.Promise;
+      mongoose.connect("mongodb://localhost/blogapp").then(()=>{
+        console.log('Conectado ao mongo..')
+      }).catch((erro) =>{
+        console.log('Erro ao se conectar: ' + erro);
+      })
 
     //public 
     app.use(express.static(path.join(__dirname, 'public'))) //informa o caminho absoluto dos arquivos staticos
@@ -26,6 +32,7 @@ const path = require('path')
 
     //Rotas
     app.use('/admin', admin)
+    
 
 //Outros
 const PORT = 8081
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,11 @@
+
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+require('../models/Categoria')
+const Categoria = mongoose.model('categorias')
+
 
 router.get('/', (req, res) =>{
     res.render('admin/index')
@@ -15,7 +21,19 @@ router.get('/categorias/add', (req, res) =>{
 })
 
 
+router.post('/categorias/nova', (req, res) =>{
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
 
+    new Categoria(novaCategoria).save().then(() =>{
+        console.log('Categoria Salva com sucesso')
+    }).catch((erro) =>{
+        console.log('Erro ao tentar salvar categoria. ' + erro);
+    })
+    
+})
 
 
 module.exports = router
